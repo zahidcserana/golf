fb2c41459dbbcd2de42af3d92de56dbbb15c2806
zahid <zahid1004048@gmail.com>
2016-08-30 18:58:06 +0600

    pass

--- resources/views/admin/change_password.blade.php
+++ resources/views/admin/change_password.blade.php
@@ -1,16 +1,69 @@
+<?php
+  $asset = asset('/');
+?>
 @extends('admin.master')
+@section('validate')
+@parent
+<script src="{{$asset}}js/jquery.min.js"></script>
+    <script src="{{$asset}}js/jquery.validate.min.js"></script>
+    <script>
+    $(document).ready(function() {
+
+        $("#passChange").validate({
+            rules: {
+                name: "required",
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                password: {
+                    required: true,
+                    minlength: 5
+                },
+                password_confirmation: {
+                    required: true,
+                    minlength: 5,
+                    equalTo: "#password"
+                },
+                email: {
+                    required: true,
+                    email: true
+                },
+            },
+            messages: {
+                name: "Please enter your firstname",
+                name: {
+                    required: "Please enter a username",
+                    minlength: "Your username must consist of at least 2 characters"
+                },
+                password: {
+                    required: "Please provide a password",
+                    minlength: "Your password must be at least 5 characters long"
+                },
+                password_confirmation: {
+                    required: "Please provide a password",
+                    minlength: "Your password must be at least 5 characters long",
+                    equalTo: "Please enter the same password as above"
+                },
+                email: "Please enter a valid email address",
+            }
+        });
+
+    });
+    </script>
+@stop
 @section('admincontent')
 <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
     <h1 class="page-header">Change Password</h1>
                     
-    <form method="POST" action="{{ url('password_reset') }}">
+    <form method="POST" id="passChange" action="{{ url('password_reset') }}">
         {!! csrf_field() !!}
         <input type="hidden" name="id" value="{{$id}}">
         <div class="form-group">
-            <input type="password" name="password" class="form-control" placeholder="Enter password">
+            <input type="password" id="password" name="password" class="form-control" placeholder="Enter password">
         </div>
         <div class="form-group">
-            <input type="password" name="password_confirmation" class="form-control" placeholder="Enter confirm password">
+            <input type="password" id="password_confirmation" name="password_confirmation" class="form-control" placeholder="Enter confirm password">
         </div>
         
         <button type="submit" class="btn btn-default">Update</button>
--- resources/views/admin/users.blade.php
+++ resources/views/admin/users.blade.php
@@ -1,11 +1,7 @@
-<?php
-    $asset = asset('/'); 
-?>
-<link href="{{$asset}}css/table.css" rel="stylesheet" type="text/css" />
 @extends('admin.master')
 @section('admincontent')
 <br /><br />
-	<table align="center">
+	<table class="table">
 		<thead>
 			<th>ID</th>
 			<th>NAME</th>
@@ -17,7 +13,7 @@
 		  <td><div>{{$user->id}} </div></td>
 		  <td><div>{{$user->name}} </div></td>
 		  <td><div>{{$user->email}} </div></td>
-		  <td><div><a href="{{route('password_change',['id'=>$user->id])}}">change_password</a> </div></td>
+		  <td><div><a href="{{route('password_change',['id'=>$user->id])}}">change-password</a> </div></td>
 		</tr>
 		@endforeach
 	</table>
