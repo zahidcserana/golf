15883da99ebbe81f41686304399a70870ba4f07a
Mahibur Rahman <mmr.akand@gmail.com>
2016-08-30 11:57:54 +0600

    delete

--- app/Http/Controllers/GolfCourseController.php
+++ app/Http/Controllers/GolfCourseController.php
@@ -10,10 +10,14 @@ class GolfCourseController extends Controller
 {
     public function Form(Request $request)
 	{
+
 		$golfCourseObj = new GolfCourse;
+		$datetime = date('Y-m-d H:i:s');
 		$userId = Session::get('userId');
 		$golfCourseObj->user_id = $userId;
 		$golfCourseObj->data = json_encode($request->gameResult);
+		$golfCourseObj->created_at = $datetime;
+		$golfCourseObj->updated_at = $datetime;
 		$golfCourseObj->save();
 		return view('index');
 	}
--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -1,21 +1,20 @@
 <?php
 session_start();
 $asset = asset('/');
-$datetime = date('Y-m-d H:i:s');
+//$datetime = date('Y-m-d H:i:s');
 echo Session::get('sessionId');
 echo Session::get('customerId');
 ?>
+<table><tr><th>Course Id</th><th>Date</th></tr>
+@foreach ($courseResult as $course)
+	<tr>
+	<div class="course">
+		<?php $timestamp = strtotime($course->created_at);
+		$date = date('d-m-Y', $timestamp);?>
 
-@foreach ($allProducts as $products)
-	<div class="product_item">
-
-		<div class="ee"><a href="{{route('product_details', ['productId'=>$products->product_id])}}"><img src="{{$asset}}{{$products->product_image}}" width="300" height="200"></div>
-
-		<div>{{$products->name}}</div></a>
-		<div>${{$products->price}}</div>
-		<div><input id="number_{{$products->product_id}}" type="text" name="quantity" value="1" size="2" />
-		<button class="addToCart" value="{{$products->product_id}}" >Add To Cart</button>
-		</div>
-
+		<td><div>{{$course->id}}</div></td>
+		<td><div>{{$date}}</div></td>
 	</div>
-	@endforeach
+	</tr>
+@endforeach
+</table>
