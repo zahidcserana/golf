9b61c608dfbcb778f8b0e52c12cdadf51561a0de
Mahibur Rahman <mmr.akand@gmail.com>
2016-09-07 20:23:11 +0600

    last

--- app/Http/Controllers/GolfCourseController.php
+++ app/Http/Controllers/GolfCourseController.php
@@ -17,7 +17,53 @@ class GolfCourseController extends Controller
 		$golfCourseObj->data = json_encode($request->gameResult);
 		$golfCourseObj->summary_data = json_encode($request->summaryResult);
 		$golfCourseObj->location = $request->nameFromUser;
-		
+
+		$golfCourseObj->par3_scoring = $request->summaryResult['par3_scoring'][0];
+		$golfCourseObj->par4_scoring = $request->summaryResult['par4_scoring'][0];
+		$golfCourseObj->par5_scoring = $request->summaryResult['par5_scoring'][0];
+		$golfCourseObj->hash_par3 = $request->summaryResult['hash_par3'][0];
+		$golfCourseObj->hash_par4 = $request->summaryResult['hash_par4'][0];
+		$golfCourseObj->hash_par5 = $request->summaryResult['hash_par5'][0];
+		$golfCourseObj->eagles = $request->summaryResult['eagles'][0];
+		$golfCourseObj->birdies = $request->summaryResult['birdies'][0];
+		$golfCourseObj->pars = $request->summaryResult['pars'][0];
+		$golfCourseObj->bogeys = $request->summaryResult['bogeys'][0];
+		$golfCourseObj->dblbogeys = $request->summaryResult['dblbogeys'][0];
+		$golfCourseObj->others = $request->summaryResult['others'][0];
+		$golfCourseObj->fairways = $request->summaryResult['fairways'][0];
+		$golfCourseObj->fir_percentage = $request->summaryResult['fir_percentage'][0];
+		$golfCourseObj->greens = $request->summaryResult['greens'][0];
+		$golfCourseObj->gir_percentage = $request->summaryResult['gir_percentage'][0];
+		$golfCourseObj->played_holes_equation = $request->summaryResult['played_holes_equation'][0];
+		$golfCourseObj->hole_rds_18 = $request->summaryResult['hole_rds_18'][0];
+		$golfCourseObj->scramble_opp_equation = $request->summaryResult['scramble_opp_equation'][0];
+		$golfCourseObj->scramble_sav_equation = $request->summaryResult['scramble_sav_equation'][0];
+		$golfCourseObj->scramble_percentage = $request->summaryResult['scramble_percentage'][0];
+		$golfCourseObj->sand_opp_equation = $request->summaryResult['sand_opp_equation'][0];
+		$golfCourseObj->sand_sav_equation = $request->summaryResult['sand_sav_equation'][0];
+		$golfCourseObj->sand_sav_percentage = $request->summaryResult['sand_sav_percentage'][0];
+		$golfCourseObj->missed_gir = $request->summaryResult['missed_gir'][0];
+		$golfCourseObj->put_1_nogir = $request->summaryResult['put_1_nogir'][0];
+		$golfCourseObj->avg_dist_chip = $request->summaryResult['avg_dist_chip'][0];
+		$golfCourseObj->total_putts = $request->summaryResult['total_putts'][0];
+		$golfCourseObj->putts_div_hole = $request->summaryResult['putts_div_hole'][0];
+		$golfCourseObj->avg_score = $request->summaryResult['avg_score'][0];
+		$golfCourseObj->putt_opp_5_equation = $request->summaryResult['putt_opp_5_equation'][0];
+		$golfCourseObj->putt_opp_5_9_equation = $request->summaryResult['putt_opp_5_9_equation'][0];
+		$golfCourseObj->putt_opp_10_19_equation = $request->summaryResult['putt_opp_10_19_equation'][0];
+		$golfCourseObj->putt_opp_20_29_equation = $request->summaryResult['putt_opp_20_29_equation'][0];
+		$golfCourseObj->putt_opp_30_equation = $request->summaryResult['putt_opp_30_equation'][0];
+		$golfCourseObj->putt_make_5_equation = $request->summaryResult['putt_make_5_equation'][0];
+		$golfCourseObj->putt_make_5_9_equation = $request->summaryResult['putt_make_5_9_equation'][0];
+		$golfCourseObj->putt_make_10_19_equation = $request->summaryResult['putt_make_10_19_equation'][0];
+		$golfCourseObj->putt_make_20_29_equation = $request->summaryResult['putt_make_20_29_equation'][0];
+		$golfCourseObj->putt_make_30_equation = $request->summaryResult['putt_make_30_equation'][0];
+		$golfCourseObj->make_percentage_5 = $request->summaryResult['make_percentage_5'][0];
+		$golfCourseObj->make_percentage_5_9 = $request->summaryResult['make_percentage_5_9'][0];
+		$golfCourseObj->make_percentage_10_19 = $request->summaryResult['make_percentage_10_19'][0];
+		$golfCourseObj->make_percentage_20_29 = $request->summaryResult['make_percentage_20_29'][0];
+		$golfCourseObj->make_percentage_30 = $request->summaryResult['make_percentage_30'][0];
+
 		if(strlen($request->dateFromUser)>0)
 			$date = \DateTime::createFromFormat('m/d/Y', $request->dateFromUser);
 		else
@@ -74,6 +120,7 @@ class GolfCourseController extends Controller
 		$golfCourseObj = new GolfCourse;
 		$editedData = json_encode($request->gameResult);
 		$editedSummaryData = json_encode($request->summaryResult);
+
 		$golfCourseObj->UpdateCourse($courseIdFromRoute, $editedData, $editedSummaryData);
 		$msg = "You have successfully updated a round.";
    		return redirect()->route('user_message', ['message'=>$msg]);
--- database/migrations/2016_09_07_120126_add_stat_to_golf_course_table.php
+++ database/migrations/2016_09_07_120126_add_stat_to_golf_course_table.php
@@ -13,7 +13,51 @@ class AddStatToGolfCourseTable extends Migration
     public function up()
     {
         Schema::table('golf_course', function (Blueprint $table) {
-            //
+            $table->double('par3_scoring');
+            $table->double('par4_scoring');
+            $table->double('par5_scoring');
+            $table->double('hash_par3');
+            $table->double('hash_par4');
+            $table->double('hash_par5');
+            $table->double('eagles');
+            $table->double('birdies');
+            $table->double('pars');
+            $table->double('bogeys');
+            $table->double('dblbogeys');
+            $table->double('others');
+            $table->double('fairways');
+            $table->double('fir_percentage');
+            $table->double('greens');
+            $table->double('gir_percentage');
+            $table->double('played_holes_equation');
+            $table->double('hole_rds_18');
+            $table->double('scramble_opp_equation');
+            $table->double('scramble_sav_equation');
+            $table->double('scramble_percentage');
+            $table->double('sand_opp_equation');
+            $table->double('sand_sav_equation');
+            $table->double('sand_sav_percentage');
+            $table->double('missed_gir');
+            $table->double('put_1_nogir');
+            $table->double('avg_dist_chip');
+            $table->double('total_putts');
+            $table->double('putts_div_hole');
+            $table->double('avg_score');
+            $table->double('putt_opp_5_equation');
+            $table->double('putt_opp_5_9_equation');
+            $table->double('putt_opp_10_19_equation');
+            $table->double('putt_opp_20_29_equation');
+            $table->double('putt_opp_30_equation');
+            $table->double('putt_make_5_equation');
+            $table->double('putt_make_5_9_equation');
+            $table->double('putt_make_10_19_equation');
+            $table->double('putt_make_20_29_equation');
+            $table->double('putt_make_30_equation');
+            $table->double('make_percentage_5');
+            $table->double('make_percentage_5_9');
+            $table->double('make_percentage_10_19');
+            $table->double('make_percentage_20_29');
+            $table->double('make_percentage_30');
         });
     }
 
@@ -25,7 +69,53 @@ class AddStatToGolfCourseTable extends Migration
     public function down()
     {
         Schema::table('golf_course', function (Blueprint $table) {
-            //
+            $table->double('par3_scoring');
+            $table->double('par4_scoring');
+            $table->double('par5_scoring');
+            $table->double('hash_par3');
+            $table->double('hash_par4');
+            $table->double('hash_par5');
+            $table->double('eagles');
+            $table->double('birdies');
+            $table->double('pars');
+            $table->double('bogeys');
+            $table->double('dblbogeys');
+            $table->double('others');
+            $table->double('fairways');
+            $table->double('fir_percentage');
+            $table->double('greens');
+            $table->double('gir_percentage');
+            $table->double('played_holes_equation');
+            $table->double('hole_rds_18');
+            $table->double('scramble_opp_equation');
+            $table->double('scramble_sav_equation');
+            $table->double('scramble_percentage');
+            $table->double('sand_opp_equation');
+            $table->double('sand_sav_equation');
+            $table->double('sand_sav_percentage');
+            $table->double('missed_gir');
+            $table->double('put_1_nogir');
+            $table->double('avg_dist_chip');
+            $table->double('total_putts');
+            $table->double('putts_div_hole');
+            $table->double('avg_score');
+            $table->double('putt_opp_5_equation');
+            $table->double('putt_opp_5_9_equation');
+            $table->double('putt_opp_10_19_equation');
+            $table->double('putt_opp_20_29_equation');
+            $table->double('putt_opp_30_equation');
+            $table->double('putt_make_5_equation');
+            $table->double('putt_make_5_9_equation');
+            $table->double('putt_make_10_19_equation');
+            $table->double('putt_make_20_29_equation');
+            $table->double('putt_make_30_equation');
+            $table->double('make_percentage_5');
+            $table->double('make_percentage_5_9');
+            $table->double('make_percentage_10_19');
+            $table->double('make_percentage_20_29');
+            $table->double('make_percentage_30');
         });
     }
 }
+
+
